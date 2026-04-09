222dd27129f1ab715f4dce412a2909db5c3ded11
sreenivasraghavendra-rgb <sreenivasraghavendra@gmail.com>
2026-04-09 12:09:12 +0530

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Portfolio from "./portfolio_website_sreenivasa_raghavendra.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Portfolio />
+  </React.StrictMode>
+);
